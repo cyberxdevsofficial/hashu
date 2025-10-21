6539d3cc288a7e4670afde8c75b8e18e2ba10a27
cyberxdevsofficial <cyberxdevsofficial@gmail.com>
2025-10-21 17:31:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ document.getElementById("contactForm")?.addEventListener("submit", function(e) {
   let name = document.getElementById("name").value;
   let message = document.getElementById("message").value;
 
-  let url = `https://wa.me/94704198014?text=Name:%20${encodeURIComponent(name)}%0A%0AMessage:%20${encodeURIComponent(message)}`;
+  let url = `https://wa.me/+94740137623?text=Name:%20${encodeURIComponent(name)}%0A%0AMessage:%20${encodeURIComponent(message)}`;
   window.open(url, "_blank");
 });
